2472f6fee4caedeea153f548dfae7d1441529489
Amir Ali Jiwani <amirali.jiwani89@gmail.com>
2016-06-07 18:04:33 +0800

    User Interface for fetching files
    Added User Interface for fetching settled/unsettled csv files from user.

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Amirali
+ * Date: 6/7/2016
+ * Time: 5:53 PM
+ */
+
+?>
+
+<html>
+<title>Settled / Unsettled Bets</title>
+<body>
+    <form action="calculate_risk.php" method="post" enctype="multipart/form-data">
+        Settled File:
+        <input type="file" name="settled_file" />
+        <br />
+        Un-Settled File:
+        <input type="file" name="unsettled_file" />
+
+        <input type="submit" value="Calculate Risk" name="submit">
+    </form>
+</body>
+</html>
