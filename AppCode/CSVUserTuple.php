65a02e0fc7d090b00226756be22ce8443a5ac29d
Amir Ali Jiwani <amirali.jiwani89@gmail.com>
2016-06-07 18:26:56 +0800

    Added CSVUserTuple
    Added CSVUserTuple to maintain the row numbers from CSV file for the
    reference to highlight later.

---
+++ AppCode/CSVUserTuple.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Amirali
+ * Date: 6/7/2016
+ * Time: 6:26 PM
+ */
+
+namespace AppCode;
+
+
+class CSVUserTuple extends UserTuple
+{
+    public $rowNum;
+}
