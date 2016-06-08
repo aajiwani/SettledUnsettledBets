47db0682bf8b5218b2bfe43671b81a61e2f20787
Amir Ali Jiwani <amirali.jiwani89@gmail.com>
2016-06-08 11:58:22 +0800

    Added risk handlers
    Created risk handlers to support Chain of responsibility design pattern

---
+++ AppCode/RiskModule/RiskIdentifier/OtherUnusualRiskHandler.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Amirali
+ * Date: 6/8/2016
+ * Time: 11:53 AM
+ */
+
+namespace AppCode\RiskModule\RiskIdentifier;
+
+class OtherUnusualRiskHandler extends RiskHandler
+{
+
+    protected function process($request)
+    {
+        // TODO: Implement process() method.
+    }
+}
