25bded74d50e5c427c3a767461a4588547ffd8fe
Amir Ali Jiwani <amirali.jiwani89@gmail.com>
2016-06-08 11:59:57 +0800

    Revert "Revert "Added risk handlers""
    This reverts commit f508152db8962b96488b41308ab23eade1318778.

---
+++ AppCode/RiskModule/RiskIdentifier/UnusualRiskHandler.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Amirali
+ * Date: 6/8/2016
+ * Time: 11:52 AM
+ */
+
+namespace AppCode\RiskModule\RiskIdentifier;
+
+class UnusualRiskHandler extends RiskHandler
+{
+    protected function process($request)
+    {
+        // TODO: Implement process() method.
+    }
+}
